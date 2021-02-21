1f180fe2a062d3873237a342e9e19b12b33f2ee0
ihaneul <789_skymert@naver.com>
2021-02-22 04:57:03 +0900

    sky

--- app/src/main/java/com/example/counteractivity/MainActivity.java
+++ app/src/main/java/com/example/counteractivity/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mTodoEditText;
     private TextView mResultTextView;
 
-//다시한번
+//다시한번 하늘아
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
